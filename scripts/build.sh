ffeadc15835fc262a42789ff3a5f615f499042b5
Cheng JIANG <cheng.jiang@outlook.fr>
2018-04-03 11:13:51 +0200

    update README.md

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,10 @@ cd $DIR
 
 echo "Building umd version"
 export NODE_ENV=development
-../node_modules/.bin/webpack-cli --hide-modules --colors --progress --config webpack.config.js --env development --display-error-details
+../node_modules/.bin/webpack --hide-modules --colors --progress --config webpack.config.js --env development --display-error-details
 
 echo "Building umd minified version"
 export NODE_ENV=production
-../node_modules/.bin/webpack-cli --hide-modules --colors --progress --config webpack.config.js --env production --display-error-details
+../node_modules/.bin/webpack --hide-modules --colors --progress --config webpack.config.js --env production --display-error-details
 
 sleep 10
